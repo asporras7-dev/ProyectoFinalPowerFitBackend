98cb7f5c6b46a17925d51dbc484d9549b13b32d5
ransesalfwdcostarica-hue <ransesalfwdcostarica@gmail.com>
2026-05-21 08:29:35 -0600

    commit

--- Front-End/src/Services/apiConfig.js
+++ Front-End/src/Services/apiConfig.js
@@ -1,3 +1,3 @@
 export const BASE_URL = "http://localhost:3001";
-export const API_BASE_URL = "http://localhost:3001/usuarios";
+export const API_BASE_URL = "http://localhost:3001";
 export const CHATBOT_URL = "http://localhost:3023";
