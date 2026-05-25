dd8de1e458244a4b0ae53b0406c01263c37f9ff0
jcob007 <jporrasfwdcostarica@gmail.com>
2026-05-25 10:09:09 -0600

    nuevo alergias

--- Front-End/src/Services/apiConfig.js
+++ Front-End/src/Services/apiConfig.js
@@ -1,3 +1,3 @@
-export const BASE_URL = "http://localhost:3000/api";
+export const BASE_URL = "http://localhost:3000";
 export const API_BASE_URL = "http://localhost:3000/api/usuarios";
 export const CHATBOT_URL = "http://localhost:3023";
